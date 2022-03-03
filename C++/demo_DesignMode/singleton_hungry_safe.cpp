34c825d3dc788d3509ea50d46dfb0ab067f118bb
Shawn696286 <18281622893@163.com>
2022-03-03 09:22:49 +0800

    更换构建方式

--- C++/demo_DesignMode/singleton_hungry_safe.cpp
+++
@@ -1,7 +0,0 @@
-#include "singleton_hungry_safe.h"
-SingletonHungry* SingletonHungry::m_pSelf = new SingletonHungry;
-
-SingletonHungry* SingletonHungry::GetInstance()
-{
-    return m_pSelf;
-}
--- C++/demo_DesignMode/singleton_lazy_reference_nosafe.cpp
+++
@@ -1,7 +0,0 @@
-#include "singleton_lazy_reference_nosafe.h"
-
-SingletonEx2& SingletonEx2::GetInstance()
-{
-    static SingletonEx2 singleton;
-    return singleton;
-}
